primecoin/primerj

commit 53d76eb27bfcfdd32a554db882172117061ff212

Expand NetType to include Bitcoin for unit testing purposes (#30)
Fix NullPointerException in unit tests related to testnet support

bitherj/src/main/java/net/bither/bitherj/core/Coin.java
@@ -82,7 +82,7 @@ public enum Coin {
             case BTW:
                 return PrimerjSettings.btwP2shHeader;
             default:
-                return PrimerjSettings.getP2shHeader();
+                return PrimerjSettings.btcP2shHeader;
         }
     }
 
@@ -97,7 +97,7 @@ public enum Coin {
             case BTW:
                 return PrimerjSettings.btwAddressHeader;
             default:
-                return PrimerjSettings.getAddressHeader();
+                return PrimerjSettings.btcAddressHeader;
         }
     }
 

bitherj/src/main/java/net/bither/bitherj/script/Script.java
@@ -27,18 +27,8 @@ import net.bither.bitherj.core.Tx;
 import net.bither.bitherj.crypto.ECKey;
 import net.bither.bitherj.crypto.TransactionSignature;
 import net.bither.bitherj.db.AbstractDb;
-import net.bither.bitherj.utils.UnsafeByteArrayOutputStream;
-import net.bither.bitherj.utils.Utils;
-
-import net.bither.bitherj.core.Coin;
-import net.bither.bitherj.core.In;
-import net.bither.bitherj.core.Out;
-import net.bither.bitherj.core.SplitCoin;
-import net.bither.bitherj.core.Tx;
-import net.bither.bitherj.crypto.ECKey;
-import net.bither.bitherj.crypto.TransactionSignature;
-import net.bither.bitherj.db.AbstractDb;
 import net.bither.bitherj.exception.ScriptException;
+import net.bither.bitherj.PrimerjSettings;
 import net.bither.bitherj.utils.UnsafeByteArrayOutputStream;
 import net.bither.bitherj.utils.Utils;
 
@@ -384,17 +374,21 @@ public class Script {
     }
 
     public String getFromAddress() throws ScriptException {
+        return getFromAddress(Utils.getNetType());
+    }
+
+    public String getFromAddress(PrimerjSettings.NetType coinType) throws ScriptException {
         if (this.chunks.size() == 2
                 && this.chunks.get(0).data != null && this.chunks.get(0).data.length > 2
                 && this.chunks.get(1).data != null && this.chunks.get(1).data.length > 2) {
-            return Utils.toAddress(Utils.sha256hash160(this.chunks.get(1).data));
+            return Utils.toAddress(Utils.sha256hash160(this.chunks.get(1).data), coinType);
         } else if (this.chunks.size() >= 3 && this.chunks.get(0).opcode == OP_0) {
             boolean isPay2SHScript = true;
             for (int i = 1; i < this.chunks.size(); i++) {
                 isPay2SHScript &= (this.chunks.get(i).data != null && this.chunks.get(i).data.length > 2);
             }
             if (isPay2SHScript) {
-                return Utils.toP2SHAddress(Utils.sha256hash160(this.chunks.get(this.chunks.size() - 1).data));
+                return Utils.toP2SHAddress(Utils.sha256hash160(this.chunks.get(this.chunks.size() - 1).data), coinType);
             }
         }
         return null;
@@ -404,8 +398,12 @@ public class Script {
      * Gets the destination address from this script, if it's in the required form (see getPubKey).
      */
     public String getToAddress() throws ScriptException {
+        return getToAddress(Utils.getNetType());
+    }
+
+    public String getToAddress(PrimerjSettings.NetType coinType) throws ScriptException {
         if (isSentToAddress())
-            return Utils.toAddress(getPubKeyHash());
+            return Utils.toAddress(getPubKeyHash(), coinType);
         else if (isSentToP2SH())
             return Utils.toP2SHAddress(this.getPubKeyHash());
         else

bitherj/src/main/java/net/bither/bitherj/ISetting.java
@@ -45,7 +45,7 @@ public abstract class ISetting {
 
     public abstract PrimerjSettings.TransactionFeePrecision getTransactionFeePrecision();
 
-    public abstract boolean isTestNet();
+    public abstract PrimerjSettings.NetType getNetType();
 
     public abstract PrimerjSettings.ApiConfig getApiConfig();
 

bitherj/src/main/java/net/bither/bitherj/PrimerjSettings.java
@@ -69,6 +69,7 @@ public class PrimerjSettings {
     public static final int addressHeader = 23;
     public static final int testNetAddressHeader = 111;
 
+    public static final int btcAddressHeader = 0;
     public static final int btgAddressHeader = 38;
     public static final int btwAddressHeader = 73;
     public static final int btfAddressHeader = 36;
@@ -76,20 +77,29 @@ public class PrimerjSettings {
 
     public static final int p2shHeader = 83;
     public static final int testNetP2shHeader = 196;
+    public static final int btcP2shHeader = 5;
     //    public static final int btgP2shHeader = 23;
     public static final int btgP2shHeader = 233;
     public static final int btwP2shHeader = 31;
     public static final int btfP2shHeader = 40;
     public static final int btpP2shHeader = 58;
 
-    public static int getAddressHeader() {
-        if(Utils.isTestNet()) return testNetAddressHeader;
-        else return addressHeader;
+    public static int getAddressHeader(NetType coinType) {
+        switch (coinType) {
+            case MAINNET: return addressHeader;
+            case TESTNET: return testNetAddressHeader;
+            case BITCOIN: return btcAddressHeader;
+            default: return addressHeader;
+        }
     }
 
-    public static int getP2shHeader() {
-        if(Utils.isTestNet()) return testNetP2shHeader;
-        else return p2shHeader;
+    public static int getP2shHeader(NetType coinType) {
+        switch (coinType) {
+            case MAINNET: return p2shHeader;
+            case TESTNET: return testNetP2shHeader;
+            case BITCOIN: return btcP2shHeader;
+            default: return p2shHeader;
+        }
     }
 
     public static long getPacketMagic() {
@@ -162,7 +172,17 @@ public class PrimerjSettings {
     public static final boolean ensureMinRequiredFee = true;
 
     public enum NetType {
-        MAINNET, TESTNET
+        MAINNET(24), TESTNET(1), BITCOIN(0); // BIP44 coin type
+
+        private int coinType;
+
+        NetType(int coinType) {
+            this.coinType = coinType;
+        }
+
+        public int getNetType() {
+            return coinType;
+        }
     }
 
     public enum TransactionFeeMode {
@@ -208,16 +228,14 @@ public class PrimerjSettings {
     }
 
     public static boolean validAddressPrefixPubkey(int pubkey) {
-        if (pubkey == getAddressHeader() || pubkey == btgAddressHeader || pubkey == btwAddressHeader ||
-                pubkey == btfAddressHeader || pubkey == btpAddressHeader) {
+        if (pubkey == addressHeader || pubkey == testNetAddressHeader || pubkey == btcAddressHeader) {
             return true;
         }
         return false;
     }
 
     public static boolean validAddressPrefixScript(int script) {
-        if (script == getP2shHeader() || script == btgP2shHeader || script == btwP2shHeader ||
-                script == btfP2shHeader || script == btpP2shHeader) {
+        if (script == p2shHeader || script == testNetP2shHeader || script == btcP2shHeader) {
             return true;
         }
         return false;

bitherj/src/main/java/net/bither/bitherj/core/HDMAddress.java
@@ -1,11 +1,10 @@
 package net.bither.bitherj.core;
 
-import net.bither.bitherj.db.AbstractDb;
-
 import net.bither.bitherj.crypto.TransactionSignature;
 import net.bither.bitherj.crypto.hd.DeterministicKey;
 import net.bither.bitherj.db.AbstractDb;
 import net.bither.bitherj.exception.PasswordException;
+import net.bither.bitherj.PrimerjSettings;
 import net.bither.bitherj.script.Script;
 import net.bither.bitherj.script.ScriptBuilder;
 import net.bither.bitherj.utils.Utils;
@@ -203,7 +202,11 @@ public class HDMAddress extends Address {
         }
 
         public String getAddress() {
-            return Utils.toP2SHAddress(Utils.sha256hash160(getMultiSigScript().getProgram()));
+            return getAddress(Utils.getNetType());
+        }
+
+        public String getAddress(PrimerjSettings.NetType coinType) {
+            return Utils.toP2SHAddress(Utils.sha256hash160(getMultiSigScript().getProgram()), coinType);
         }
     }
 

bitherj/src/main/java/net/bither/bitherj/core/Out.java
@@ -17,8 +17,6 @@
 package net.bither.bitherj.core;
 
 import net.bither.bitherj.PrimerjSettings;
-
-import net.bither.bitherj.PrimerjSettings;
 import net.bither.bitherj.crypto.ECKey;
 import net.bither.bitherj.exception.ProtocolException;
 import net.bither.bitherj.exception.ScriptException;
@@ -157,10 +155,14 @@ public class Out extends Message {
     }
 
     public String getOutAddress() {
+        return getOutAddress(Utils.getNetType());
+    }
+
+    public String getOutAddress(PrimerjSettings.NetType coinType) {
         if (outAddress == null) {
             try {
                 Script pubKeyScript = new Script(this.getOutScript());
-                outAddress = pubKeyScript.getToAddress();
+                outAddress = pubKeyScript.getToAddress(coinType);
             } catch (ScriptException e) {
 //                if (this.getOutScript() != null) {
 //                    log.warn("out script : " + Utils.bytesToHexString(this.getOutScript()));

bitherj/src/main/java/net/bither/bitherj/core/SplitCoin.java
@@ -142,7 +142,7 @@ public enum SplitCoin {
     public int getP2shHeader() {
         switch (this) {
             case BCC:
-                return PrimerjSettings.getP2shHeader();
+                return PrimerjSettings.btcP2shHeader;
             case BTG:
                 return PrimerjSettings.btgP2shHeader;
             case BTW:
@@ -152,13 +152,13 @@ public enum SplitCoin {
             case BTP:
                 return PrimerjSettings.btpP2shHeader;
         }
-        return PrimerjSettings.getP2shHeader();
+        return PrimerjSettings.btcP2shHeader;
     }
 
     public int getAddressHeader() {
         switch (this) {
             case BCC:
-                return PrimerjSettings.getAddressHeader();
+                return PrimerjSettings.btcAddressHeader;
             case BTG:
                 return PrimerjSettings.btgAddressHeader;
             case BTW:
@@ -168,7 +168,7 @@ public enum SplitCoin {
             case BTP:
                 return PrimerjSettings.btpAddressHeader;
         }
-        return PrimerjSettings.getAddressHeader();
+        return PrimerjSettings.btcAddressHeader;
     }
 
     public String getIsGatKey() {

bitherj/src/main/java/net/bither/bitherj/crypto/ECKey.java
@@ -19,9 +19,7 @@ package net.bither.bitherj.crypto;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 
-import net.bither.bitherj.utils.Sha256Hash;
-import net.bither.bitherj.utils.Utils;
-
+import net.bither.bitherj.PrimerjSettings;
 import net.bither.bitherj.utils.Sha256Hash;
 import net.bither.bitherj.utils.Utils;
 
@@ -360,6 +358,10 @@ public class ECKey implements Serializable {
      */
     public String toAddress() {
         return Utils.toAddress(Utils.sha256hash160(pub));
+    }
+
+    public String toAddress(PrimerjSettings.NetType coinType) {
+        return Utils.toAddress(Utils.sha256hash160(pub), coinType);
     }
 
     /**

commit 411ef4df8347b15cf2d41c0eec5f286bd2b626f8

Fix an issue where transaction out amount and balance remaining are displayed incorrectly (Broken since 53d76eb27bfcfdd32a554db882172117061ff212) Minor fix of script getToAddress()

bitherj/src/main/java/net/bither/bitherj/core/Coin.java
@@ -1,8 +1,6 @@
 package net.bither.bitherj.core;
 
 import net.bither.bitherj.PrimerjSettings;
-
-import net.bither.bitherj.PrimerjSettings;
 import net.bither.bitherj.crypto.TransactionSignature;
 import net.bither.bitherj.utils.Utils;
 
@@ -82,7 +80,7 @@ public enum Coin {
             case BTW:
                 return PrimerjSettings.btwP2shHeader;
             default:
-                return PrimerjSettings.btcP2shHeader;
+                return PrimerjSettings.getP2shHeader(Utils.getNetType());
         }
     }
 
@@ -97,7 +95,7 @@ public enum Coin {
             case BTW:
                 return PrimerjSettings.btwAddressHeader;
             default:
-                return PrimerjSettings.btcAddressHeader;
+                return PrimerjSettings.getAddressHeader(Utils.getNetType());
         }
     }
 

bitherj/src/main/java/net/bither/bitherj/script/Script.java
@@ -405,7 +405,7 @@ public class Script {
         if (isSentToAddress())
             return Utils.toAddress(getPubKeyHash(), coinType);
         else if (isSentToP2SH())
-            return Utils.toP2SHAddress(this.getPubKeyHash());
+            return Utils.toP2SHAddress(this.getPubKeyHash(), coinType);
         else
             throw new ScriptException("Cannot cast this script to a pay-to-address type");
     }