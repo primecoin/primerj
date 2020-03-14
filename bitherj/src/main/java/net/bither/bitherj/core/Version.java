6904fc0d30498e1b80271f66be333703ef4d0506
ankang1 <ankang1@v.systems>
2020-03-14 18:44:42 +0000

    version to 0.2.0

--- bitherj/src/main/java/net/bither/bitherj/core/Version.java
+++ bitherj/src/main/java/net/bither/bitherj/core/Version.java
@@ -17,6 +17,6 @@
 package net.bither.bitherj.core;
 
 public class Version {
-    public static final String version = "0.1.0";
+    public static final String version = "0.2.0";
     public static final String name = "Primerj";
 }
