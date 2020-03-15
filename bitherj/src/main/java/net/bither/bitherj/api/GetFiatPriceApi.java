7d1b219d09c3977b3fc5e0453b2e88414e148fb6
ankang1 <ankang1@v.systems>
2020-03-15 17:15:25 +0000

    change file name

--- bitherj/src/main/java/net/bither/bitherj/api/GetFiatPriceApi.java
+++ bitherj/src/main/java/net/bither/bitherj/api/GetFiatPriceApi.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2014 http://Bither.net
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 package net.bither.bitherj.api;
 
@@ -21,7 +6,7 @@ import net.bither.bitherj.api.http.PrimerUrl;
 
 import org.json.JSONObject;
 
-public class GetSimplePriceApi extends HttpGetResponse<String> {
+public class GetFiatPriceApi extends HttpGetResponse<String> {
 
     private static final String CNYPRICE = "cny";
     private static final String USDPRICE = "usd";
@@ -30,8 +15,8 @@ public class GetSimplePriceApi extends HttpGetResponse<String> {
     private float mCny;
     private float mUsd;
 
-    public GetSimplePriceApi() {
-        setUrl(PrimerUrl.BITHER_SIMPLE_PRICE);
+    public GetFiatPriceApi() {
+        setUrl(PrimerUrl.PRIMER_FIAT_PRICE_URL);
     }
 
     @Override
