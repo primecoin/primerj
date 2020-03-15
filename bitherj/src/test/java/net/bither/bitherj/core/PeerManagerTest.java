67ae20a028944557fb6ce43af3d5b2904599631e
Sunny King <sunnyking9999@gmail.com>
2020-03-15 00:00:36 +0000

    Skip unit tests requiring database implementation

--- bitherj/src/test/java/net/bither/bitherj/core/PeerManagerTest.java
+++ bitherj/src/test/java/net/bither/bitherj/core/PeerManagerTest.java
@@ -16,10 +16,12 @@
 
 package net.bither.bitherj.core;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PeerManagerTest {
 
+    @Ignore("Database required")
     @Test
     public void testNormal() throws InterruptedException {
         Block block = new Block(2, "00000000000000000ee9b585e0a707347d7c80f3a905f48fa32d448917335366", "4d60e37c7086096e85c11324d70112e61e74fc38a5c5153587a0271fd22b65c5", 1400928750
